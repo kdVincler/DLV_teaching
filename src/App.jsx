bca6a66cef4d6d9631e2b71a754afd2fdcd15dc5
kdVincler <konrad.vincler@gmail.com>
2024-08-07 12:06:22 +0100

    Adding animation for the hamburger icon

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,16 @@ function App() {
             nav ul {
               display: ${hamburgerOpen ? 'inline' : 'none'};
             }
+            .burger1 {
+              transform: ${hamburgerOpen ? 'rotate(45deg)' : 'rotate(0)'};
+            }
+            .burger2 {
+              // transform: ${hamburgerOpen ? 'translateX(100%)' : 'translateX(0)'};
+              opacity: ${hamburgerOpen ? 0 : 1}
+            }
+            .burger3 {
+              transform: ${hamburgerOpen ? 'rotate(-45deg)' : 'rotate(0)'};
+            }
           }
         `}</style>
 
