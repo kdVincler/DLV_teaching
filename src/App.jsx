6dedd2d9da205cd1383e3d115b801e18d708bbd5
kdVincler <konrad.vincler@gmail.com>
2025-12-25 20:18:38 +0100

    svg and text size fixes

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
             </ul>
           </div>
           <a 
-            className="font-extrabold text-2xl font-[Great_Vibes] pl-4 md:text-4xl"
+            className="font-extrabold text-xs font-[Great_Vibes] pl-4 md:text-4xl"
             onClick={() => {setShowing(0)}}
           >
               Doroti Vincler
@@ -67,11 +67,11 @@ function App() {
         </div>
         <div className="navbar-end">
           <a 
-            className="btn btn-outline hover:bg-neutral-900/20 hover:border-black hover:shadow-none text-xl font-semibold dark:text-black"
+            className="btn btn-outline hover:bg-neutral-900/20 hover:border-black hover:shadow-none text-xs md:text-xl font-semibold dark:text-black"
             onClick={() => {setShowing(4)}}
           >
             Contact
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-id-card-lanyard-icon lucide-id-card-lanyard"><path d="M13.5 8h-3"/><path d="m15 2-1 2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h3"/><path d="M16.899 22A5 5 0 0 0 7.1 22"/><path d="m9 2 3 6"/><circle cx="12" cy="15" r="3"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-id-card-lanyard-icon lucide-id-card-lanyard"><path d="M13.5 8h-3"/><path d="m15 2-1 2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h3"/><path d="M16.899 22A5 5 0 0 0 7.1 22"/><path d="m9 2 3 6"/><circle cx="12" cy="15" r="3"/></svg>
           </a>
         </div>
       </div>
